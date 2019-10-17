f87e462126c57ae7b80fa5c912827231887353c5
Zaky98 <zakyluthfansa@gmail.com>
2019-10-18 01:23:08 +0700

    menambahkan homescreen, about, dan category

--- app/src/main/java/com/example/e_cinema/activities/MainActivity.java
+++ app/src/main/java/com/example/e_cinema/activities/MainActivity.java
@@ -1,16 +1,65 @@
 package com.example.e_cinema.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.e_cinema.R;
+import com.example.e_cinema.fragements.AboutFragment;
+import com.example.e_cinema.fragements.CategoryFragment;
+import com.example.e_cinema.fragements.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+//    private void loadFragment(FoodFragment foodFragment) {
+//    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.action_category:
+                fragment = new CategoryFragment();
+                break;
+            case R.id.action_info:
+                fragment = new AboutFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
